1f4045c2803625a64c19d1923e2f07d4b32e31f9
Paul <paul@elliptic.co>
2021-01-24 16:01:30 +0000

    so using ddodle seems to work but its fairly slow, Ill just make a PNG the normal way I guess

--- project/Deps.scala
+++ project/Deps.scala
@@ -2,23 +2,27 @@ import sbt.Keys.libraryDependencies
 import sbt._
 
 object Deps {
-  val circeVersion   = "0.12.3"
-  val catsVersion    = "2.2.0"
-  val refinedVersion = "0.9.20"
+  val catsVersion           = "2.2.0"
+  val circeVersion          = "0.12.3"
+  val consoleForCatsVersion = "0.8.1"
+  val doodleVersion         = "0.9.20"
+  val refinedVersion        = "0.9.20"
 
   lazy val deps = {
     libraryDependencies ++= List(
       // Cats
-      "org.typelevel"  %% "cats-core"     % catsVersion,
-      "org.typelevel"  %% "cats-effect"   % catsVersion,
-      "dev.profunktor" %% "console4cats"  % "0.8.1",
+      "org.typelevel"     %% "cats-core"     % catsVersion,
+      "org.typelevel"     %% "cats-effect"   % catsVersion,
+      "dev.profunktor"    %% "console4cats"  % consoleForCatsVersion,
       // Circe
-      "io.circe"       %% "circe-core"    % circeVersion,
-      "io.circe"       %% "circe-literal" % circeVersion,
-      "io.circe"       %% "circe-parser"  % circeVersion,
-      "io.circe"       %% "circe-generic" % circeVersion,
+      "io.circe"          %% "circe-core"    % circeVersion,
+      "io.circe"          %% "circe-literal" % circeVersion,
+      "io.circe"          %% "circe-parser"  % circeVersion,
+      "io.circe"          %% "circe-generic" % circeVersion,
+      // Doodle
+      "org.creativescala" %% "doodle"        % doodleVersion,
       // Refined Types
-      "eu.timepit"     %% "refined"       % refinedVersion
+      "eu.timepit"        %% "refined"       % refinedVersion
     )
   }
 }
---
+++ src/main/scala/chladni/DoodleMain.scala
@@ -0,0 +1,43 @@
+package chladni
+
+import doodle.image._
+import doodle.image.syntax._
+import doodle.core._
+import doodle.java2d.java2dPngWriter
+import doodle.effect.Writer._
+
+object DoodleMain extends App {
+
+  val coords = (BigDecimal(-1.0) to BigDecimal(1.0) by BigDecimal(0.005)).map(_.toDouble)
+
+  val q          = makeSquareGrid(coords)(EigenFunctions.w(5, 6))
+  val normalised = normalise(q)
+
+  val finalImage = normalised.map(_.map(valueToSquare).reduce(_ beside _)).reduce(_ above _)
+
+  finalImage.write[Png]("hello.png")
+
+  def getColour(value: Double): Color =
+    math.floor(30 * math.abs(value)) match {
+      case 0 => Color.aliceBlue
+      case _ => Color.black
+    }
+
+  def normalise(grid: List[List[Double]]): List[List[Double]] = {
+    val absoluteMax = math.max(math.abs(grid.flatten.max), math.abs(grid.flatten.min))
+    grid.map(_.map(_ / absoluteMax))
+  }
+
+  def valueToSquare(value: Double) =
+    Image
+      .square(1)
+      .strokeWidth(0.0)
+      .fillColor(getColour(value))
+
+  private def makeSquareGrid(coords: IndexedSeq[Double])(f: (Double, Double) => Double): List[List[Double]] =
+    (for { x <- coords; y <- coords } yield f(x, y))
+      .grouped(coords.length)
+      .toList
+      .map(_.toList)
+
+}
