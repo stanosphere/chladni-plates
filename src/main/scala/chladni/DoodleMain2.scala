4253c4ceceb4f8e07ab6d50143df05501522c22a
Paul <paul@elliptic.co>
2021-01-24 16:39:00 +0000

    time all the things

--- src/main/scala/chladni/DoodleMain2.scala
+++ src/main/scala/chladni/DoodleMain2.scala
@@ -5,22 +5,39 @@ import java.io.File
 import javax.imageio.ImageIO
 import java.awt.image.BufferedImage
 
-object DoodleMain2 extends App {
-  val coords = (BigDecimal(-1.0) to BigDecimal(1.0) by BigDecimal(0.001)).map(_.toDouble)
+object Timed {
+  def apply[A](name: String)(block: => A): A = {
+    val start = System.currentTimeMillis()
+    val res   = block
+    val end   = System.currentTimeMillis()
+    println(s"$name took ${end - start} ms to run")
+    res
+  }
+}
 
-  val q                = makeSquareGrid(coords)(EigenFunctions.w(3, 8))
-  val normalisedValues = normalise(q)
+object DoodleMain2 extends App {
+  val coords = Timed("making coord array")((BigDecimal(-1.0) to BigDecimal(1.0) by BigDecimal(0.001)).map(_.toDouble))
+
+  val q                = Timed("evaluating function to make value")(makeSquareGrid(coords)(EigenFunctions.w(3, 8)))
+  val normalisedValues = Timed("normilisation")(normalise(q))
   val flattenedCoords  =
-    for {
-      (ys, x)    <- normalisedValues.map(_.zipWithIndex).zipWithIndex
-      (value, y) <- ys
-    } yield (x, y, value)
+    Timed("flattening down the coords") {
+      for {
+        (ys, x)    <- normalisedValues.map(_.zipWithIndex).zipWithIndex
+        (value, y) <- ys
+      } yield (x, y, value)
+    }
 
   val out = new BufferedImage(q.length, q.length, BufferedImage.TYPE_INT_RGB)
 
-  flattenedCoords.foreach { case (x, y, value) => out.setRGB(x, y, getColour(value)) }
+  Timed("updateing the bufferedimage") {
+    flattenedCoords.foreach { case (x, y, value) => out.setRGB(x, y, getColour(value)) }
 
-  ImageIO.write(out, "png", new File("test.png"))
+  }
+
+  Timed("writing the image to file") {
+    ImageIO.write(out, "png", new File("test.png"))
+  }
 
   def getColour(value: Double) =
     math.floor(30 * math.abs(value)) match {
