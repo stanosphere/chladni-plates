dbd496f011ed5b71eb709e1b5f5451e776b80a1c
Paul <paul@elliptic.co>
2021-01-24 17:41:17 +0000

    remove doodle stuff

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,6 @@ object Deps {
   val catsVersion           = "2.2.0"
   val circeVersion          = "0.12.3"
   val consoleForCatsVersion = "0.8.1"
-  val doodleVersion         = "0.9.20"
   val refinedVersion        = "0.9.20"
 
   lazy val deps = {
@@ -19,8 +18,6 @@ object Deps {
       "io.circe"          %% "circe-literal" % circeVersion,
       "io.circe"          %% "circe-parser"  % circeVersion,
       "io.circe"          %% "circe-generic" % circeVersion,
-      // Doodle
-      "org.creativescala" %% "doodle"        % doodleVersion,
       // Refined Types
       "eu.timepit"        %% "refined"       % refinedVersion
     )
