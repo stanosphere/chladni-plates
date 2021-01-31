335e6a8753ecd5e7219254246ad3200742ef06d0
Paul <paul@elliptic.co>
2021-01-31 00:17:13 +0000

    generate random linear combination

--- src/main/scala/chladni/RandomDrawing.scala
+++ src/main/scala/chladni/RandomDrawing.scala
@@ -2,17 +2,27 @@ package chladni
 
 import chladni.utils.GenEnrichment._
 import org.scalacheck._
+import EigenFunctions._
 
 object RandomDrawing extends App {
 
-  val myGen = for {
-    a <- Gen.choose[Int](-12, 30)
-    b <- Gen.choose[Int](678, 4230)
-    c <- Gen.choose[Int](90, 100)
-  } yield a + b * c
+  val genEigenFunction: Gen[(Double, Double) => Double] =
+    for {
+      m     <- Gen.choose[Int](1, 10)
+      n     <- Gen.choose[Int](1, 10)
+      coeff <- Gen.choose[Double](-1, 1)
+    } yield (x: Double, y: Double) => coeff * u(m)(x) * u(n)(y)
 
-  println { myGen.sampleWithSeed(10) }
-  println { myGen.sampleWithSeed(10) }
-  println { myGen.sampleWithSeed(10) }
+  val genLinearCombination: Gen[(Double, Double) => Double] =
+    for {
+      length <- Gen.choose[Int](2, 10)
+      fs     <- Gen.listOfN(length, genEigenFunction)
+    } yield (x: Double, y: Double) => fs.foldLeft(0d)(_ + _(x, y))
+
+  val seed = 65437
+
+  println { genLinearCombination.sampleWithSeed(seed).get(0.3, 0.4) }
+  println { genLinearCombination.sampleWithSeed(seed).get(0.3, 0.4) }
+  println { genLinearCombination.sampleWithSeed(seed).get(0.3, 0.4) }
 
 }
